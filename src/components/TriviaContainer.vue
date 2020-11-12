ad17f3d9bf8ecaef23fb4469b357b8a2225e0859
Heso113 <hampe_olsson91@hotmail.com>
2020-11-12 15:38:22 +0100

    feature
    fetches 10 random boardgame questions from the API and presents them one by one to the user along with the possible answers.

---
+++ src/components/TriviaContainer.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <!-- <h3 v-if="fetchedTrivias">Completed trivias: {{getCompletedTriviaCount(fetchedTrivias)}}</h3> -->
+    <!-- <h3 v-if="currentTrivia">Trivia: {{currentTrivia}}</h3> -->
+    <TriviaItems @next-trivia="nextTrivia" v-bind:trivias="fetchedTrivias" v-bind:index="index" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import TriviaItems from './TriviaItems.vue'
+
+export default {
+  name: 'TriviaContainer',
+  components: {TriviaItems},
+  data: () => ({
+    fetchedTrivias: null,
+    index: '0',
+    correctAnswers: '0'
+  }),
+  mounted() {
+    axios
+     .get('https://opentdb.com/api.php?amount=10&category=16&difficulty=easy')
+     .then(response => this.fetchedTrivias = response.data.results)
+  },
+  methods: {
+    nextTrivia: function(answer) {
+      if (this.fetchedTrivias[this.index].correct_answer === answer) {
+        this.correctAnswers++;
+      }
+      console.log(this.fetchedTrivias[this.index].correct_answer);
+      console.log('Number of correct answers: ' + this.correctAnswers);
+      this.index++;
+      if (this.index >= 10) {
+        this.index = 0;
+        this.testFunction();
+      }
+      console.log(answer);
+      console.log(this.index);
+    },
+    testFunction: function() {
+      console.log('This is a test. Hello!');
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/TriviaItems.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h3>{{trivias[index].question}}</h3>
+        <button v-on:click="nextTrivia(trivias[index].correct_answer)">{{trivias[index].correct_answer}}</button>
+        <button v-on:click="nextTrivia(trivias[index].incorrect_answers[0])">{{trivias[index].incorrect_answers[0]}}</button>
+        <button v-if="trivias[index].type === 'multiple'" v-on:click="nextTrivia(trivias[index].incorrect_answers[1])">{{trivias[index].incorrect_answers[1]}}</button>
+        <button v-if="trivias[index].type === 'multiple'" v-on:click="nextTrivia(trivias[index].incorrect_answers[2])">{{trivias[index].incorrect_answers[2]}}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TriviaItems',
+    props: ['trivias', 'index'],
+    methods: {
+        populateDetails() {
+            this.$emit("populate-details");
+        },
+        nextTrivia(answer) {
+            this.$emit("next-trivia", answer);
+        }
+    }
+
+}
+</script>
+
+<style>
+    ul {
+        list-style: none;
+    }
+    li {
+        margin-bottom: 1rem;
+    }
+    .crossed {
+        text-decoration: line-through;
+    }
+    button {
+        background-color: black;
+        color: red;
+        width: 200px;
+        height: 50px;
+    }
+</style>

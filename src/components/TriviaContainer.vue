bf76f5a17d3217e8b09b6f682184a5b215958846
Heso113 <hampe_olsson91@hotmail.com>
2020-11-13 15:53:56 +0100

    feature
    Fixed so that the answers are randomised when displayed to the user.

--- src/components/TriviaContainer.vue
+++ src/components/TriviaContainer.vue
@@ -2,7 +2,7 @@
   <div>
     <TriviaItems
       @next-trivia="nextTrivia"
-      v-bind:trivias="fetchedTrivias"
+      v-bind:trivias="trivias"
       v-bind:index="index"
     />
   </div>
@@ -16,11 +16,25 @@ export default {
   components: { TriviaItems },
   data: () => ({
     fetchedTrivias: null,
+    trivias: [],
     index: "0",
   }),
   mounted() {
     this.index = 0;
     this.fetchedTrivias = this.$store.getters.fetchedTrivias;
+    let trivias = [];
+    this.fetchedTrivias.forEach(t => {
+      let newTrivia = {trivia: t.question, answers: [], type: t.type};
+      let a = [];
+      a.push(t.correct_answer);
+      t.incorrect_answers.forEach(incorrect => {
+        a.push(incorrect);
+      })
+      this.shuffleArray(a);
+      newTrivia.answers = a;
+      trivias.push(newTrivia); 
+    });
+    this.trivias = trivias;
   },
   methods: {
     nextTrivia: function (answer) {
@@ -46,6 +60,23 @@ export default {
         index++;
       });
       this.$store.commit("setScore", score);
+    },
+    shuffleArray(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      } 
+      return array;
     },
   },
 };
